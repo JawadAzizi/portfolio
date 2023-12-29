7bef9ec051bb6df08936d3c48a573336f5eb35b2
Jawad Azizi <102347277+JawadAzizi@users.noreply.github.com>
2023-12-29 08:46:25 +0430

    not important

--- app/libs/components/home.tsx
+++ app/libs/components/home.tsx
@@ -5,12 +5,17 @@ export function Main() {
         <div className="w-[50%]">
           <img src="./jawad.jpg" alt="jawad phont" />
         </div>
-        <div className="w-[50%]">
+        <div className="w-[50%] p-2">
           <h1 className="text-2xl">Mohammad Jawad Azizi</h1>
           <p>Software engineer | Full-stack web developer</p>
-          <p className="p-4">I am an skilled software engineer and full-stack web developer with 2 years of 
-            web development experience. making easy to use easy to use features and user 
-            systems was one of the experience that i got during the work.
+          <p className="p-4">I am an skilled software engineer and with 2 years of 
+            web development experience. I Have an easy to use features and and user friendly 
+            developement approach to frontend development was one of the experience that i got during the work.
+            dkflk;fja;l
+
+
+
+            
           </p>
         </div>
       </div>
